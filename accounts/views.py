9701a30db593ccf5d16f64dc8bfdd308c4a6f086
KaranPatel186 <69085636+KaranPatel186@users.noreply.github.com>
2021-02-26 11:13:36 +0000

    Upload project

---
+++ accounts/views.py
@@ -0,0 +1,60 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import inlineformset_factory
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+from .forms import CreateUserForm
+
+
+def home(request):
+    return render(request, 'accounts/moodtracker.html')
+
+
+def counselling(request):
+    return render(request, 'accounts/counselling.html')
+
+
+def meditation(request):
+    return render(request, 'accounts/meditation.html')
+
+
+def moodtracker(request):
+    return render(request, 'accounts/moodtracker.html')
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('../moodtracker')
+        else:
+            messages.info(request, 'Username OR Password is incorrect')
+
+    context = {}
+    return render(request, 'accounts/loginPage.html')
+
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account Created Successfully')
+
+            return redirect('../login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
